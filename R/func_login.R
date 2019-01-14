e054e96ca0800243603a07b41818bb3b15d8b78b
S Daniel Zafar <daniel.zafar@navigant.com>
2019-01-14 11:54:15 -0600

    assorted updates

--- R/func_login.R
+++ R/func_login.R
@@ -1,18 +1,24 @@
-NAVlogin <- function(un, pass) {
-  require(magrittr)
-  require(rvest)
-
-  url <- "https://www.insidenci.com/psp/paprd/?cmd=login"
-  session <- html_session(url)
-
-  login_form <- session %>%
-    html_node("form[name=login]") %>%
-    html_form() %>%
-    set_values(
-      userid = un,
-      pwd = pass
-    )
-
-  session %>%
-    submit_form(form = login_form, submit = "Submit")
-}
+# NAVlogin <- function(un) {
+#   require(magrittr)
+#   require(rvest)
+#
+#
+#   pass <- rstudioapi::showPrompt(
+#     "Authenication",
+#     paste0("Enter Navigant password for user '", un, "'"),
+#     default = "")
+#
+#   url <- "https://www.insidenci.com/psp/paprd/?cmd=login"
+#   session <- html_session(url)
+#
+#   login_form <- session %>%
+#     html_node("form[name=login]") %>%
+#     html_form() %>%
+#     set_values(
+#       userid = un,
+#       pwd = pass
+#     )
+#
+#   session %>%
+#     submit_form(form = login_form, submit = "Submit")
+# }
